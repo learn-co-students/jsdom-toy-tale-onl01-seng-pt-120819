6c25a6b7adfe155c2c5412317f44ed2c221de557
github username <github email address>
2020-06-26 13:37:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,58 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
+      toyFormContainer.addEventListener("submit", (event) => {
+        event.preventDefault()
+        // console.log(event.target.name.value)
+        fetchPostRequest(event.target)
+      })
     } else {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const toyCollectionDiv = document.querySelector("#toy-collection")
+
+  fetch("http://localhost:3000/toys")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      // console.log(object);
+      object.forEach(element => {
+        const div = document.createElement("div")
+        div.class = "card"
+        div.innerHTML += `<h2>${element.name}</h2>`
+        div.innerHTML += `<img src="${element.image}" class="toy-avatar">`
+        div.innerHTML += `<p>Likes: ${element.likes}</p>`
+        const likeButton = document.createElement("button")
+        likeButton.class = "like-btn"
+        likeButton.innerHTML = "Like"
+        toyCollectionDiv.appendChild(div)
+        toyCollectionDiv.appendChild(likeButton)
+      })
+    })
+
+    function fetchPostRequest(event) {
+      console.log(event)
+      fetch("http://localhost:3000/toys", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name: event.name.value,
+          image: event.image.value,
+          likes: 0 
+        })
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object);
+      })
+    }
+
 });
